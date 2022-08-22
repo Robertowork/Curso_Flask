f8ed7625deea5db76cbf4eee12c6ca502f4ae6a7
Roberto Junior <roberto.junior054@gmailcom>
2022-08-22 16:54:40 -0300

    adição de um formulario

--- Curso_Flask_2022_2/aula_2/teste_de_rotas/app.py
+++ Curso_Flask_2022_2/aula_2/teste_de_rotas/app.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask("projeto")
 @app.route("/")
 def ola_mundo():
@@ -12,7 +12,23 @@ def ola_mundo():
     return render_template("alo.html", n=nome, aProdutos=produtos), 200
 # Nova Rota Teste
 @app.route("/teste")
-@app.route("/teste/<carro_do_leite>")
-def funcao_teste(carro_do_leite = ""):
-    return "Nova rota teste<br>variável: {}".format(carro_do_leite), 200
+@app.route("/teste/<teste_de_rota>")
+def funcao_teste(teste_de_rota = ""):
+    return "Nova rota teste<br>variável: {}".format(teste_de_rota), 200
+
+#Rota do formulario
+@app.route("/form")
+def form():
+    return render_template("form.html"), 200
+
+#rota de tratamento do formulario
+
+@app.route("/form_recebe", methods=["GET","POST"])
+def form_recebe():
+    if request.method == "POST":
+        nome = request.form["Roberto"]
+        return "Nome: {}" .format(nome), 200
+    else:
+        return "Não pode chamar direto no GET", 200
+
 app.run() 
