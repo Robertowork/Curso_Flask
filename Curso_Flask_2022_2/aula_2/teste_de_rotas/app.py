1af25d3bdfeed845bd311a76e6def860d63f08a4
Roberto Junior <roberto.junior054@gmailcom>
2022-08-22 15:40:38 -0300

    passagem de informaçoes pela nova rota

--- Curso_Flask_2022_2/aula_2/teste_de_rotas/app.py
+++ Curso_Flask_2022_2/aula_2/teste_de_rotas/app.py
@@ -1,15 +1,18 @@
+# coding: utf-8
 from flask import Flask, render_template
 app = Flask("projeto")
 @app.route("/")
 def ola_mundo():
+    #criar uma variável com o meu nome
     nome="Seu Nome"
     produtos = [
     {"nome": "Ração", "preco": 199.99},
     {"nome": "Playstation", "preco": 1999.99}]
-    return render_template("alo.html", n=nome, aProdutos=produtos), 200
-    
-@app.router("/teste")
-def função_teste():
-    return "Nova rota teste", 200
 
-app.run()
+    return render_template("alo.html", n=nome, aProdutos=produtos), 200
+# Nova Rota Teste
+@app.route("/teste")
+@app.route("/teste/<carro_do_leite>")
+def funcao_teste(carro_do_leite):
+    return "Nova rota teste<br>variável:{}".format(carro_do_leite), 200
+app.run() 
