613ea8966be830dfcfeb9ef310f9b1f33f2da478
Roberto Junior <roberto.junior054@gmailcom>
2022-08-22 15:03:42 -0300

    adição de nova rota

--- Curso_Flask_2022_2/aula_2/teste_de_rotas/app.py
+++ Curso_Flask_2022_2/aula_2/teste_de_rotas/app.py
@@ -1,14 +1,15 @@
-# coding: utf-8
 from flask import Flask, render_template
-tabela = Flask("projeto")
-@tabela.route("/")
+app = Flask("projeto")
+@app.route("/")
 def ola_mundo():
-    #criar uma variável com o meu nome
     nome="Seu Nome"
     produtos = [
     {"nome": "Ração", "preco": 199.99},
     {"nome": "Playstation", "preco": 1999.99}]
-
     return render_template("alo.html", n=nome, aProdutos=produtos), 200
+    
+@app.router("/teste")
+def função_teste():
+    return "Nova rota teste", 200
 
-tabela.run()
+app.run()
